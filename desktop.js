99fc4363796fb2c8d3541f93de06eed90a6aa822
aidanbeck <ambt3y@umsystem.edu>
2024-05-13 18:54:29 -0500

    Updated version with DL assets

--- desktop.js
+++ desktop.js
@@ -1,34 +1,113 @@
-const wrapper = document.getElementsByClassName("DESKTOP-DIV")[0];
+var randomImages = [
 
-let tilePixels = 70;
-let columns = Math.floor(document.body.clientWidth / tilePixels);
-let rows = Math.floor(document.body.clientHeight / tilePixels);
-rows = columns;
+    //rocks
+    "<img class='icon-image rock move-glow' src='images/rock.png'>",
+    "<img class='icon-image rock move-glow' src='images/rock2.png'>",
+    "<img class='icon-image rock move-glow' src='images/rock3.png'>",
+
+    //plants
+    "<img class='icon-image sapling default-glow' src='images/sapling.png'>",
+    "<img class='icon-image shrub default-glow' src='images/shrub.png'>",
+    "<img class='icon-image tree default-glow' src='images/tree.png'>",
+
+    //unnatural
+    "<img class='icon-image statue hostile-glow' src='images/statue.png'>",
+    "<img class='icon-image ned player-glow' src='images/ned.png'>",
+    "<img class='icon-image grave default-glow' src='images/grave.png'>",
+    "<img class='icon-image coyote hostile-glow' src='images/coyote.gif'>",
+    "<img class='icon-image rat hostile-glow' src='images/rat.gif'>",
+    "<img class='icon-image sack hostile-glow' src='images/uhhhh.gif'>",
+
+    //reversed
+    "<img class='icon-image rock move-glow reversed' src='images/rock.png'>",
+    "<img class='icon-image rock move-glow reversed' src='images/rock2.png'>",
+    "<img class='icon-image rock move-glow reversed' src='images/rock3.png'>",    
+]
+
+var iconCount = 0;
 
 function createTile() {
     const tile = document.createElement("div");
     tile.classList.add("tile");
-    //tile.draggable = true;
+
+    const icon = document.createElement("div");
+    icon.classList.add("icon");
+
+    icon.addEventListener("dragstart", pickupIcon);
+    icon.addEventListener("drop", dropIcon);
+    icon.addEventListener("dragover", hoverTileWhileDragging);
+    icon.addEventListener("dragleave", exitTileWhileDragging);
+
+    var random = Math.random()
+    if (random > 0.5) {
+        tile.classList.add("dirt");
+    }
+
+    if (random > 0.995) {
+        icon.draggable = true;
+        let index = Math.floor(Math.random() * randomImages.length);
+        
+        if (index == 6 || index == 7 || index == 8) {
+            if (random > 0.999999) {
+                index = 6; //rare statue
+            } else {
+                index = 5; //tree
+            }
+            
+        }
+
+        icon.innerHTML = randomImages[index];
+        icon.firstChild.style.zIndex = getZIndex(icon) * 2;
+    }
+
+    if (iconCount == 16+16+16+2) {
+        icon.draggable = true;
+        icon.innerHTML = randomImages[8]; //gravestone
+    }
+    iconCount++;
+
+    icon.onclick = function() {
+
+        icon.style.backgroundColor = "";
+
+        if (icon.innerHTML == "") {
+            icon.draggable = true;
+            let index = Math.floor(Math.random() * randomImages.length);
+            if (index == 8) { //grave
+                index = 3; //sapling
+            }
+            icon.innerHTML = randomImages[index];
+            icon.firstChild.style.zIndex = getZIndex(icon) * 2; //why *2?
+            icon.firstChild.src = icon.firstChild.src + '?id=' + Math.floor(Math.random() * 100); //animation offset. Could be better way?
+
+        } else {
+            icon.innerHTML = "";
+            icon.draggable = false;
+        }
+    }
+
+    tile.appendChild(icon);
     
     return tile;
 }
 
-function createTiles(quantity) {
+function getZIndex(element) {
+
+    var computedStyle = window.getComputedStyle(element);
+    var zIndex = computedStyle.getPropertyValue('z-index');
+    return parseInt(zIndex); //may not need
+}
+
+function createTiles(wrapper, columns, rows) {
 
     for (i = 0; i < rows; i++) {
         for (j=0; j < columns; j++) {
             let tileElement = createTile();
             
-            tileElement.style.zIndex = i*columns + (rows-j);
+            //const zIndex = i*rows + (columns-j);
 
-            tileElement.onclick = function() {
-
-                if (this.innerHTML == "") {
-                    this.innerHTML = "<img class='icon' src='images/ned.png'>";
-                } else {
-                    this.innerHTML = "";
-                }
-            }
+            //tileElement.style.zIndex = i + (1/j);
+            tileElement.firstChild.style.zIndex = i + 2; //nessecary so that the tile underneath will not get dragged along with the icon. I have no idea why that happens and it took me hours to figure this out.
             wrapper.appendChild(tileElement);
         }
         
@@ -36,21 +115,50 @@ function createTiles(quantity) {
 
 }
 
-createTiles(columns * rows);
 
-const createGrid = () => {
+const createGrid = (wrapper,columns,rows,scale) => {
     
-    wrapper.innerHTML = "";
-    columns = Math.floor(document.body.clientWidth / tilePixels);
-    rows = Math.floor(document.body.clientHeight / tilePixels);
-    rows = columns;
-
     wrapper.style.setProperty("--columns", columns);
     wrapper.style.setProperty("--rows", rows);
+    wrapper.style.setProperty("height", scale*rows);
 
-    createTiles(columns * rows);
+    wrapper.style.height = scale*rows + "px";
+    wrapper.style.width = scale*columns + "px";
+
+    createTiles(wrapper, columns, rows);
 }
 
-window.onresize = () => createGrid();
+const chunk0 = document.getElementsByClassName("DESKTOP-DIV")[0]; //chunk0.style.zIndex = 0; //this number will be chunk y level
+const chunk1 = document.getElementsByClassName("DESKTOP-DIV")[1]; //chunk1.style.zIndex = 0;
+const chunk2 = document.getElementsByClassName("DESKTOP-DIV")[2]; //chunk2.style.zIndex = 1;
 
-createGrid();
+const chunk3 = document.getElementsByClassName("DESKTOP-DIV")[3]; chunk3.style.display = "none";
+const chunk4 = document.getElementsByClassName("DESKTOP-DIV")[4]; chunk4.style.display = "none";
+const chunk5 = document.getElementsByClassName("DESKTOP-DIV")[5]; chunk5.style.display = "none";
+
+const chunk6 = document.getElementsByClassName("DESKTOP-DIV")[6]; chunk6.style.display = "none";
+const chunk7 = document.getElementsByClassName("DESKTOP-DIV")[7]; chunk7.style.display = "none";
+const chunk8 = document.getElementsByClassName("DESKTOP-DIV")[8]; chunk8.style.display = "none";
+
+
+var worldscale = camera.scale;
+createGrid(chunk0, 16, 16, worldscale);
+createGrid(chunk1, 16, 16, worldscale);
+createGrid(chunk2, 16, 16, worldscale);
+
+createGrid(chunk3, 16, 16, worldscale);
+createGrid(chunk4, 16, 16, worldscale);
+createGrid(chunk5, 16, 16, worldscale);
+
+createGrid(chunk6, 16, 16, worldscale);
+createGrid(chunk7, 16, 16, worldscale);
+createGrid(chunk8, 16, 16, worldscale);
+
+function scaleChunks(columns,rows,scale) {
+    const elements = document.getElementsByClassName("DESKTOP-DIV");
+
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.height = scale*rows + "px";
+        elements[i].style.width = scale*columns + "px";
+    }
+}
---
+++ drag.js
@@ -0,0 +1,105 @@
+//fired while mouse is hovering over a tile (while dragging)
+//this is necessary because dragging cancels the standard hover events
+function hoverTileWhileDragging(ev) {
+    
+    ev.preventDefault(); //not sure why *exactly* this is needed, but it gets rid of the 🚫 cursor, and dragging fails without it
+    ev.target.classList.add("dragging-over");
+}
+
+//fired when the mouse exits the bounds of a tile (while dragging)
+function exitTileWhileDragging(ev) {
+    ev.target.classList.remove("dragging-over");
+}
+  
+//fired when icon is picked up
+function pickupIcon(ev) {
+
+    let data = "draggingid"; //this will eventually be the tile # type and it's location
+    ev.dataTransfer.setData("text", data); //I could also store this in my own global variable instead
+
+    //Sets the ID of the pickup tile to "draggingid" so the element can be searched later
+    ev.target.id = data; 
+}
+
+//fired when icon is dropped
+function dropIcon(ev) {
+    var data = ev.dataTransfer.getData("text"); //get data stored in drag (currently always "draggingid")
+
+    //finds element with id "draggingid", then clones it
+    let draggedIcon = document.getElementById(data);
+    let clonedIcon = draggedIcon.cloneNode(true);
+
+    //place cloned icon at target
+    ev.target.innerHTML = "";
+    ev.target.draggable = true;
+    ev.target.classList.remove("dragging-over");
+    ev.target.appendChild(clonedIcon.firstChild);
+
+    //delete original icon
+    draggedIcon.innerHTML = "";
+    draggedIcon.draggable = false;
+    draggedIcon.removeAttribute('id');
+}
+
+//simple arrow key movement
+var camera = {
+    x:0,
+    y:0,
+    scale: 90, //worldscale
+    element: document.body
+}
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowUp' || event.key === 'w') {
+        camera.y += camera.scale;
+    } else if (event.key === 'ArrowDown' || event.key === 's') {
+        camera.y -= camera.scale;
+    } else if (event.key === 'ArrowLeft' || event.key === 'a') {
+        camera.x += camera.scale;
+    } else if (event.key === 'ArrowRight' || event.key === 'd') {
+        camera.x -= camera.scale;
+    }
+
+    camera.element.style.top = camera.y + 'px';
+    camera.element.style.left = camera.x + 'px';
+
+    // if (camera.y <= -1401) {
+    //     document.getElementsByClassName("DESKTOP-DIV")[0].style.visibility = "hidden";
+    //     document.getElementsByClassName("DESKTOP-DIV")[1].style.visibility = "hidden";
+    //     document.getElementsByClassName("DESKTOP-DIV")[2].style.visibility = "hidden";
+
+    //     document.getElementsByClassName("DESKTOP-DIV")[6].style.visibility = "visible";
+    //     document.getElementsByClassName("DESKTOP-DIV")[7].style.visibility = "visible";
+    //     document.getElementsByClassName("DESKTOP-DIV")[8].style.visibility = "visible";
+    // } else {
+    //     document.getElementsByClassName("DESKTOP-DIV")[0].style.visibility = "visible";
+    //     document.getElementsByClassName("DESKTOP-DIV")[1].style.visibility = "visible";
+    //     document.getElementsByClassName("DESKTOP-DIV")[2].style.visibility = "visible";
+
+    //     document.getElementsByClassName("DESKTOP-DIV")[6].style.visibility = "hidden";
+    //     document.getElementsByClassName("DESKTOP-DIV")[7].style.visibility = "hidden";
+    //     document.getElementsByClassName("DESKTOP-DIV")[8].style.visibility = "hidden";
+    // }
+});
+
+
+//Scrolling & zooming
+window.addEventListener("wheel", function(event) {
+
+    const zoomStep = 8;
+
+    if (event.deltaY > 0) {
+        camera.scale-=camera.scale/zoomStep;
+    } else {
+        camera.scale+=camera.scale/zoomStep;
+    }
+
+    if (camera.scale > 400) {
+        camera.scale = 400;
+    }
+    else if (camera.scale < 35) {
+        camera.scale = 35;
+    }
+
+    scaleChunks(16,16,camera.scale);
+    
+});
